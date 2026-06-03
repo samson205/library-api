9bcb7f0ed72568167edea62f59d164c70a89a5ff
samson205 <134064060+samson205@users.noreply.github.com>
2026-06-03 19:20:46 +0300

    Fixed bug

--- app/users/dependencies.py
+++ app/users/dependencies.py
@@ -11,10 +11,10 @@ from app.auth.services import UserService
 oauth2_schema = OAuth2PasswordBearer(tokenUrl="auth/login")
 
 
-async def get_current_user(token: str = Depends(oauth2_schema), db: AsyncSession = Depends(get_db)):
+async def get_current_user(token: str = Depends(oauth2_schema), db: AsyncSession = Depends(get_db)) -> User:
     payload = decode_token(token, "access")
     result = await db.scalars(
-        select(User).where(User.email == payload["email"], User.is_active == True)
+        select(User).where(User.email == payload["sub"], User.is_active == True)
     )
     user = result.first()
     if not user:
@@ -26,5 +26,14 @@ async def get_current_user(token: str = Depends(oauth2_schema), db: AsyncSession
     return user
 
 
-async def get_user_service(db: AsyncSession = Depends(get_db)):
+async def get_current_admin(user: User = Depends(get_current_user)) -> User:
+    if user.role != "admin":
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail="Only admins can perform this action"
+        )
+    return user
+
+
+async def get_user_service(db: AsyncSession = Depends(get_db)) -> UserService:
     return UserService(db)
